fb65babe217456864b88a54eddcb29cf558e8d36
Veeresh5965 <137785946+Veeresh5965@users.noreply.github.com>
2025-05-06 09:31:52 +0530

    Update Program_01.java

--- src/com/test/Program_01.java
+++ src/com/test/Program_01.java
@@ -4,6 +4,10 @@ public class Program_01 {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("Enter the number:");
+		int n=sc.nextInt();
+		System.out.println("Square of the "+n+"is"+n*n);
+			
 	}
 
 }
