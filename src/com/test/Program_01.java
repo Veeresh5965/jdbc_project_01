b367964c6630cdf03c0c01fa5a55068d8cba37ba
Veere <Veere@LAPTOP-CDQ2HPCV.lan>
2025-05-06 09:38:29 +0530

    updated the code

--- src/com/test/Program_01.java
+++ src/com/test/Program_01.java
@@ -1,12 +1,15 @@
 package com.test;
 
+import java.util.Scanner;
+
 public class Program_01 {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 		System.out.println("hello world");
 		System.out.println("Enter the number:");
 		int n=sc.nextInt();
-		System.out.println("Square of the "+n+"is"+n*n);
+		System.out.println("Square of the "+n+" is " +n*n);
 			
 	}
 
